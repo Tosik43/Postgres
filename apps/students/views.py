9b92340fbec17c06a3b1601486441001dff836ae
Maxim Tarasov <tosik4302@mail.ru>
2026-08-05 17:46:40 +0500

    ci: add simple table for students

--- apps/students/views.py
+++ apps/students/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Student
+
+
+def student_list(request):
+
+    students = Student.objects.all()
+
+    return render(
+        request,
+        "students/student_list.html",
+        {
+            "students": students
+        }
+    )
